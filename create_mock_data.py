8ff53250ebabd280482a40293136abbdf041b9d4
MjdMahasneh <m.almahasneh@lboro.ac.uk>
2023-11-20 14:31:59 +0000

    new files

--- create_mock_data.py
+++ create_mock_data.py
@@ -8,7 +8,7 @@ from PIL import Image, ImageDraw
 def random_color():
     return tuple(np.random.choice(range(256), size=3))
 
-def create_mock_dataset(base_dir, num_images=10, image_size=(256, 256)):
+def create_mock_dataset(base_dir, num_images=10, image_size=(256, 256), more_circles_and_squares=False):
     # Define the paths
     train_image_path = os.path.join(base_dir, 'train', 'images')
     train_mask_path = os.path.join(base_dir, 'train', 'masks')
@@ -29,7 +29,11 @@ def create_mock_dataset(base_dir, num_images=10, image_size=(256, 256)):
         mask_draw = ImageDraw.Draw(mask)
 
         # Add shapes
-        for _ in range(np.random.randint(1, 6)):  # Random number of shapes
+        num_shapes = np.random.randint(1, 6)  # Random number of shapes
+        if more_circles_and_squares:
+            num_shapes += 2  # Increase the number of shapes
+
+        for _ in range(num_shapes):
             shape_type = np.random.choice(['circle', 'rectangle'])
             shape_size = np.random.randint(20, 70)  # Random shape size
             x1, y1 = np.random.randint(0, image_size[0] - shape_size, size=2)
@@ -41,9 +45,6 @@ def create_mock_dataset(base_dir, num_images=10, image_size=(256, 256)):
             elif shape_type == 'rectangle':
                 draw.rectangle([x1, y1, x2, y2], fill=random_color())
                 mask_draw.rectangle([x1, y1, x2, y2], fill=2)  # Class 2
-            # elif shape_type == 'triangle':
-            #     draw.polygon([(x1, y1), (x2, y2), (x1, y2)], fill=random_color())
-            #     mask_draw.polygon([(x1, y1), (x2, y2), (x1, y2)], fill=3)
 
         # Add random noise
         noise = np.random.randint(0, 50, image_size + (3,))  # Noise intensity
@@ -51,26 +52,16 @@ def create_mock_dataset(base_dir, num_images=10, image_size=(256, 256)):
         noisy_image = np.clip(noisy_image, 0, 255)  # Ensure values are within RGB range
         image = Image.fromarray(noisy_image.astype('uint8'))
 
-        ## show image and mask side by side
-        # fig, ax = plt.subplots(1, 2)
-        # ax[0].imshow(image)
-        # ax[0].set_title('image')
-        # ax[1].imshow(mask)
-        # ax[1].set_title('mask')
-        # plt.show()
-
         # Save the image and mask
         image.save(os.path.join(folder, 'images', f'image_{image_id}.jpg'))
         mask.save(os.path.join(folder, 'masks', f'image_{image_id}.png'))
 
     # Create images and masks for training and validation sets
     for i in range(num_images):
-
-        if i < .8 * num_images:
+        if i < int(0.8 * num_images):
             create_image_and_mask(i, os.path.join(base_dir, 'train'))
-
-        if i < .2 * num_images:
+        if i < int(0.2 * num_images):
             create_image_and_mask(i, os.path.join(base_dir, 'val'))
 
 # Usage
-create_mock_dataset('./mock_dataset', num_images=1000, image_size=(256, 256))
+create_mock_dataset('./mock_dataset', num_images=1000, image_size=(256, 256), more_circles_and_squares=True)
